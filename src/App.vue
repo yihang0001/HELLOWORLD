556010a42a15e3dcd3428bd3e6bdfddc0a317863
caiyihang <517511131@qq.com>
2020-03-19 23:07:23 +0800

    beta0.005

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img src="https://cai-glsc.oss-cn-hangzhou.aliyuncs.com/406/AS%60E5C%5BV88T%24F459%29LOCD%7B7.png"><br/>
     <h2>永远相信美好的事情即将发生</h2><br/>
-     <h2>祝 发财!</h2><br/>
+     <h2>祝 爵士 政委 渣男 体面人 发财!</h2><br/>
      <h2>---来自 居家等死无业游民蔡某</h2>
   </div>
 </template>
